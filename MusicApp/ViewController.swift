584fb62d097429f7580d3fbee8f7b83ff1b78b5c
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2022-03-22 22:42:34 +0600

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Акбала Тлеугалиева on 3/22/22.
+//  Copyright © 2022 Akbala Tleugaliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
